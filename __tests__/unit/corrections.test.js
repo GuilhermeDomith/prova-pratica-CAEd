f86f5fd68185bb44026d401bdfc16c1c3a928996
Guilherme Domith <guilhermedomith@gmail.com>
2020-04-14 03:54:42 -0300

    [UP] tests: send keys to correct

--- __tests__/unit/corrections.test.js
+++ __tests__/unit/corrections.test.js
@@ -1,11 +1,11 @@
 const request = require('supertest');
 
 const app = require('../../src/app');
-const { truncade } = require('../utils/database');
 const factory = require('../utils/factories');
+const { truncade } = require('../utils/database');
 const { Correction } = require('../../src/model/correction.model');
 
-describe('Corrections', () => {
+describe('Order by Corrections', () => {
     beforeAll(async (done) => {
         await truncade();
 
@@ -17,32 +17,79 @@ describe('Corrections', () => {
         done();
     })
 
-    it('Should get the next item to be corrected', async (done) => {
-        const response = await request(app)
-            .get('/correcoes/proxima');
+    it('Should get the first item', async (done) => {
+        const first = this.corrections[0];
 
+        const response = await request(app).get('/correcoes/proxima');
         const { data } = response.body;
-        console.log(data);
-        expect(data.ordem).toBe(this.corrections[0].ordem);
+
+        expect(data.ordem).toBe(first.ordem);
+        expect(data.id).toBe(first.id);
         done()
     });
 
-    it('Should correct the item', async (done) => {
-        const response = await request(app)
-            .post(`/correcoes/${this.corrections[0]._id}`);
+    it('Should correct the first item', async (done) => {
+        const first = this.corrections[0];
+        const keysCorrected = first.chave.map((key) => {
+            return {
+                id: key.id,
+                valor: key.opcoes[0].valor
+            }
+        });
 
-        console.log(`/correcoes/${this.corrections[0]._id}`);
+        const response = await request(app)
+            .post(`/correcoes/${first.id}`)
+            .send({ chave: keysCorrected });
+
         expect(response.status).toBe(200);
         done()
     });
 
-    it('Should get the next item to be corrected', async (done) => {
-        const response = await request(app)
-            .get('/correcoes/proxima');
+    it('Should not correct third item', async (done) => {
+        const third = this.corrections[2];
 
+        const response = await request(app)
+            .post(`/correcoes/${third.id}`);
+
+        expect(response.status).not.toBe(200);
+        done()
+    });
+
+    it('Should reserve second item', async (done) => {
+        const second = this.corrections[1];
+
+        const response = await request(app)
+            .post(`/correcoes/reservadas/${second.id}`);
+
+        expect(response.status).toBe(200);
+        done()
+    });
+
+    it('Should get the third item', async (done) => {
+        const third = this.corrections[2];
+
+        const response = await request(app).get('/correcoes/proxima');
         const { data } = response.body;
-        console.log(data);
-        expect(data.ordem).toBe(this.corrections[1].ordem);
+
+        expect(data.ordem).toBe(third.ordem);
+        expect(data.id).toBe(third.id);
+        done()
+    });
+
+    it('Should correct the third item', async (done) => {
+        const third = this.corrections[2];
+        const keysCorrected = third.chave.map((key) => {
+            return {
+                id: key.id,
+                valor: key.opcoes[0].valor
+            }
+        });
+
+        const response = await request(app)
+            .post(`/correcoes/${third.id}`)
+            .send({ chave: keysCorrected });
+
+        expect(response.status).toBe(200);
         done()
     });
 
